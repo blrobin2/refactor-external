99ad4b86df26ab1627ed5a4c354544cafa40f331
Bennie <brobinson88@gmail.com>
2018-07-05 13:00:34 -0400

    Refactor access to YouTube data through Gateway

--- VideoService.ts
+++ VideoService.ts
@@ -3,33 +3,24 @@ import { youtube_v3 } from 'googleapis'
 
 import Video from './Video'
 import YouTubeConnection from './YouTubeConnection'
+import YouTubeGateway from './YouTubeGateway'
 
 export default class VideoService {
   async videoList(): Promise<string> {
     const videoList: Video[] = JSON.parse(readFileSync('videos.json', 'utf8'))
     const ids = videoList.map((v: any) => v.youtube_id)
-    const youtube = new YouTubeConnection
-    const items = await youtube.listVideos(ids)
+    const items = await (new YouTubeConnection).listVideos(ids)
+    const youtube = new YouTubeGateway(items)
+
     ids.forEach((id: string) => {
       const video = videoList.find(v => id === v.youtube_id)
       if (!video) return
-      const youtubeRecord = items.find(v => id === v.id)
-      if (!youtubeRecord) return
-      const viewCount = this.getViewCount(youtubeRecord)
-      video.views = parseInt(viewCount, 10)
-      const publishedAt = this.getPublishedAt(youtubeRecord)
+      video.views = parseInt(youtube.item(id).views, 10)
+      const publishedAt = youtube.item(id).published
       const daysAvailable = Date.now() - Date.parse(publishedAt)
       video.monthlyViews = video.views ? (video.views * 365.0/daysAvailable/12) : 0
     })
 
     return JSON.stringify(videoList)
-  }
-
-  private getViewCount(youtubeRecord?: youtube_v3.Schema$Video): string {
-    return (youtubeRecord && youtubeRecord.statistics && youtubeRecord.statistics.viewCount) || "0";
-  }
-
-  private getPublishedAt(youtubeRecord?: youtube_v3.Schema$Video): string {
-    return (youtubeRecord && youtubeRecord.snippet && youtubeRecord.snippet.publishedAt) || new Date().toLocaleString()
   }
 }
